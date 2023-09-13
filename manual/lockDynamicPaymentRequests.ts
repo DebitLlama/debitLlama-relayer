4ff73a72d223a6080e548171b0621a31ad4b7b1f
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-09-13 09:08:33 +0200

    added manual relayer running

---
+++ manual/lockDynamicPaymentRequests.ts
@@ -0,0 +1,21 @@
+import "$std/dotenv/load.ts";
+
+import { createClient } from "@supabase/supabase-js";
+import { lockDynamicRequests } from "../businessLogic/actions.ts";
+import QueryBuilder from "../db/queryBuilder.ts";
+
+async function main() {
+  console.log("processCreatedFixedPayments start");
+
+  const client = createClient(
+    Deno.env.get("SUPABASE_URL") || "",
+    Deno.env.get("SUPABASE_KEY") || "",
+    { auth: { persistSession: false } },
+  );
+
+  const queryBuilder = new QueryBuilder(client);
+
+  await lockDynamicRequests(queryBuilder);
+}
+
+await main();
