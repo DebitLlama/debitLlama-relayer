ace9fba71a85da017c41c56982393bf30c74b0ef
StrawberryChocolateFudge <hopeter@protonmail.com>
2023-12-09 04:05:49 +0000

    for deploy

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import "$std/dotenv/load.ts";
 
-import "./scheduler/cron.ts";
-import { getRelayerBalances } from "./web3/web3.ts";
+// import "./scheduler/cron.ts";
+// import { getRelayerBalances } from "./web3/web3.ts";
 
-await getRelayerBalances();
+// await getRelayerBalances();
